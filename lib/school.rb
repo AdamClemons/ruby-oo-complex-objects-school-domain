140faacb12af677a46288e17817bb0acf473aa9e
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-10-07 20:59:08 -0400

    commit tree 515a6e295ff07c8354d8472b5598689d68e6205d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}        
+    end
+
+    def add_student(student, year)
+        roster[year] ||= []
+        roster[year] << student        
+    end
+
+    def grade(year)
+        roster[year]      
+    end
+
+    def sort
+        sorted = {}
+
+        roster.each do |year, students|
+            # binding.pry
+            sorted[year] = students.sort 
+        end
+        sorted        
+    end
+
+
+end
